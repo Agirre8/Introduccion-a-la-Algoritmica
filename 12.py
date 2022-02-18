c710ed221c84bb70fea7f20ee585a42561af122d
Agirre8 <91331083+Agirre8@users.noreply.github.com>
2022-02-18 23:09:30 +0100

    Add files via upload

---
+++ 12.py
@@ -0,0 +1,16 @@
+from xmlrpc.client import boolean
+
+
+saldo: int
+cantidad_retirar: int
+cantidad_depositar: int
+
+depositar = saldo + cantidad_depositar
+if saldo>0
+    retirar = saldo - cantidad_retirar
+
+cliente_premium: boolean
+
+if cliente_premium True
+    retirar = saldo - cantidad_retirar
+
